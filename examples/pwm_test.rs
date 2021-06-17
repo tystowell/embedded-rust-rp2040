bb931f81e13a66e652291e4cb30e868ddf35f573
Tyler Stowell <tylerrstowell@gmail.com>
2021-06-17 11:44:18 -0600

    Updating pwm_test using the embedded c examples from pico-sdk. It still doesn't work, though I'm fairly sure my initialization process is now identical to theirs, so it must be close.

--- examples/pwm_test.rs
+++ examples/pwm_test.rs
@@ -18,6 +18,8 @@ fn main() -> ! {
     let io = p.IO_BANK0;
     let pads = p.PADS_BANK0;
 
+
+
     p.RESETS.reset.modify(|_, w| w.pwm().clear_bit());
 
     while p.RESETS.reset_done.read().pwm().bit_is_clear() {}
@@ -28,23 +30,41 @@ fn main() -> ! {
 
     pads.gpio[0].reset();
 
-    io.gpio[0].gpio_ctrl.write_with_zero(|w| w.funcsel().pwm_a_0());
 
-    //pwm.ch0_csr.write(|w| w.ph_correct().set_bit());
 
-    pwm.ch0_top.write(|w| unsafe { w.ch0_top().bits(0xfffe) });
+    pwm.ch0_csr.write(|w| w.ph_correct().clear_bit()); //Disable ph_correct
 
-    //pwm.ch0_cc.write(|w| unsafe { w.b().bits(0x7fff) });
+    pwm.ch0_div.write(|w| unsafe { w.int().bits(0x01u8) }); //Set clock divider to 1
 
-    pwm.ch0_csr.write(|w| w.en().set_bit());
+    pwm.ch0_csr.write(|w| w.divmode().div()); //Sets counter to go at rate dictated by fractional divider (not gated)
+
+    pwm.ch0_csr.write(|w| w.a_inv().clear_bit()); //Don't invert channel A
+
+    pwm.ch0_csr.write(|w| w.b_inv().clear_bit()); //Don't invert channel B
+
+    pwm.ch0_top.write(|w| unsafe { w.ch0_top().bits(0xffffu16) }); //Set TOP register to max value
+
+
+    pwm.ch0_cc.write(|w| unsafe { w.a().bits(0x7fffu16) }); //Default duty cycle of 50%
+
+    pwm.ch0_ctr.write(|w| unsafe { w.ch0_ctr().bits(0x0000u16) }); //Reset the counter
+
+    pwm.ch0_csr.write(|w| w.en().set_bit()); // Enable the PWM channel
+
+    // Pad input enable and output disable take preference over any peripherals using it.
+    // According to pico-sdk, enabling inputs and outputs through the pad lets the peripheral control it's own behavior.
+    pads.gpio[0].write(|w| w.ie().set_bit());
+    pads.gpio[0].write(|w| w.od().clear_bit());
+
+    io.gpio[0].gpio_ctrl.write_with_zero(|w| w.funcsel().pwm_a_0()); // Connect the gpio pin with the pwm
 
     loop {
-        pwm.ch0_cc.write(|w| unsafe { w.a().bits(0x0000) });
+        pwm.ch0_cc.write(|w| unsafe { w.a().bits(0x4000u16) });
 
-        cortex_m::asm::delay(500_000);
+        cortex_m::asm::delay(900_000);
 
-        pwm.ch0_cc.write(|w| unsafe { w.a().bits(0xffff) });
+        pwm.ch0_cc.write(|w| unsafe { w.a().bits(0xa000u16) });
 
-        cortex_m::asm::delay(500_000);
+        cortex_m::asm::delay(900_000);
     }
 }
